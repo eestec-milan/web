b1d47ea03bee1fcbf888ecf7c745317df44e1daf
TA50 <awadalbrkal@gmail.com>
2022-12-03 11:20:47 +0100

    add db migrations

---
+++ database/migrations/2022_11_26_153326_create_attendances_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->string("member_id")->foreign('member_id')->references('id')->on('members');
+            $table->string("meeting_id")->foreign('meeting_id')->references('id')->on('meetings');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
